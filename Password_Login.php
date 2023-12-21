2aade8e87d59a25536ca81c7bed2710e42a9b662
Fvkyeah <154470350+Fvkyeah@users.noreply.github.com>
2023-12-21 17:41:34 +0100

    Update Password_Login.php

--- Password_Login.php
+++ Password_Login.php
@@ -1,1 +1,27 @@
 
+<?php
+// Check if the email is set and not empty
+if (isset($_POST['email']) && !empty($_POST['email'])) {
+    $email = $_POST['email'];
+} else {
+    // Redirect back to the email login page if the email is not provided
+    header("Location: email_login.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Password Page</title>
+</head>
+<body>
+    <h2>Password Page</h2>
+    <form method="post" action="process_login.php">
+        <input type="hidden" name="email" value="<?php echo $email; ?>">
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password" required><br><br>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
